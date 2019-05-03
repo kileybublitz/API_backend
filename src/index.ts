df30938d91461282e411e01e590cb3013387d43b
Kiley Adams <kileyadams92@gmail.com>
2019-05-03 11:48:38 -0500

    add CRUD routes and env, update start

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import {getTodos, getTodoById, createTodo, updateTodo, deleteTodo} from './queries'
 
 const app = express()
 const port = 3000
@@ -7,13 +8,19 @@ const port = 3000
 app.use(bodyParser.json())
 app.use(
     bodyParser.urlencoded({
-        extended: true
+        extended: false
     })
 )
 
 app.get('/', (request, response) => {
 response.json({info: 'Test'})
 })
+
+app.get('/todos', getTodos)
+app.get('/todos/:id', getTodoById)
+app.post('/todos', createTodo)
+app.put('/todos/:id', updateTodo)
+app.delete('/todos/:id', deleteTodo)
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`)
---
+++ src/queries.ts
@@ -0,0 +1,68 @@
+import { Pool } from 'pg'
+import { Response, Request } from 'express'
+import { config } from 'dotenv';
+
+var c = config({path: '../.env'});
+
+const pool = new Pool({
+    database: process.env.PGDATABASE,
+    user: process.env.PGUSER,
+    host: 'localhost',
+    password: process.env.PGPASSWORD, 
+    port: 5432
+})
+
+export const getTodos = (request: Request, response: Response) => {
+    pool.query('SELECT * FROM todos ORDER BY id ASC', (error, results) => {
+        if(error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    }) 
+}
+
+export const getTodoById = (request: Request, response: Response) => {
+    const id = parseInt(request.params.id)
+    pool.query('SELECT * FROM todos WHERE id = $1', [id], (error, results) => {
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+export const createTodo = (request: Request, response: Response) => {
+    const {title} = request.body
+
+    pool.query('INSERT INTO todos (title) VALUES ($1)', [title], (error, results) => {
+        if(error){
+            throw error
+        }
+        response.status(201).send(`Todo added with ID: ${results}`)
+    })
+}
+
+export const updateTodo = (request: Request, response: Response) => {
+    const id = parseInt(request.params.id)
+    const {title, completed} = request.body
+
+    pool.query('UPDATE todos SET title = $1, completed = $2 WHERE id = $3',
+    [title, completed, id], 
+    (error, results) => {
+        if(error) {
+            throw error
+        }
+        response.status(200).send(`Todo modified with ID: ${id}`)
+    })
+}
+
+export const deleteTodo = (request: Request, response: Response) => {
+    const id = parseInt(request.params.id)
+
+    pool.query('DELETE FROM todos WHERE id = $1', [id], (error, results) => {
+        if(error){
+            throw error
+        }
+        response.status(200).send(`Todo deleted with ID: ${id}`)
+    })
+}
