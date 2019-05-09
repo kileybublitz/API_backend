3f274b7cf24fa0b6ae3fe799c02bc2e02cfbf6f6
KileyAdams92 <31576550+KileyAdams92@users.noreply.github.com>
2019-05-09 09:20:44 -0500

    Revert "add CRUD routes and env, update start"

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,19 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import {getTodos, getTodoById, createTodo, updateTodo, deleteTodo} from './queries'
-import { config } from 'dotenv';
-
-config();
 
 const app = express()
-const port = 5000
+const port = 3000
 
 app.use(bodyParser.json())
 app.use(
     bodyParser.urlencoded({
-        extended: false
+        extended: true
     })
 )
 
 app.get('/', (request, response) => {
 response.json({info: 'Test'})
 })
-
-app.get('/todos', getTodos)
-app.get('/todos/:id', getTodoById)
-app.post('/todos', createTodo)
-app.put('/todos/:id', updateTodo)
-app.delete('/todos/:id', deleteTodo)
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`)
